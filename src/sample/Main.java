dc84e51104ed7655563f6848ccbb0e5eb0392511
mtafar <mtanus_messi@hotmail.com>
2020-04-22 15:34:19 +0200

    Update Main.java

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,18 +1,22 @@
 package sample;
-
+import java.util.List;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
+import javafx.scene.layout.VBox;
 public class Main extends Application {
-
+private parent content(){
+    VBox root = new VBox();
+    root.setsize(600 ,500);
+    return root;
+}
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(content()));
         primaryStage.show();
     }
 
